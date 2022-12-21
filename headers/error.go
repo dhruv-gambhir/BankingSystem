d36db7ef1476e16245a77a8876e3fe5685016704
Dhruv Gambhir <75164918+dhruv-gambhir@users.noreply.github.com>
2022-12-21 14:10:23 +0530

    Delete headers directory

--- headers/error.go
+++
@@ -1,7 +0,0 @@
-package headers
-
-func CheckError(err error) {
-    if err != nil {
-        panic(err)
-    }
-}
--- headers/sqlconsts.go
+++
@@ -1,9 +0,0 @@
-package headers
-
-const (
-    host     = "localhost"
-    port     = 5432
-    user     = "banker"
-    password = "dhruv123"
-    dbname   = "postgres"
-)
